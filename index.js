4be600be5fb3b31d7611d45f9c6b9df19f4ea63d
robarcand <robbyarcand@aol.com>
2019-11-07 11:51:41 -0500

    modified index.js

--- index.js
+++ index.js
@@ -2,25 +2,19 @@
 
 const apiKey = '348839-SimpleRe-U2PY34VF';
 
+const options = {
+    method: 'GET',
+    mode: 'no-cors'
+}
 
 function getRecommendation() {
-    // $.ajax({
-    //     type: 'GET',
-    //     url: 'https://tastedive.com/api/similar?q=the+smiths&k=348839-SimpleRe-U2PY34VF',
-    //     jsonp: 'callback',
-    //     dataType: 'jsonp'
-    // })
 
     const url = `https://tastedive.com/api/similar?q=the+smiths&k=${apiKey}`;
-
-    const options = {
-        headers: new Headers({
-            "X-Api-Key": apiKey})
-        };
 
     fetch(url, options)
     .then(response => response.json())
     .then(responseJson => console.log(responseJson))
+    // .catch(error => alert('Something went wrong.'))
 }
 
 function watchForm() {
