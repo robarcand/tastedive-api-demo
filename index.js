7fc0d49f23c54b0891298085c941e65541beabd9
robarcand <robbyarcand@aol.com>
2019-11-07 19:21:34 -0500

    latest commit

--- index.js
+++ index.js
@@ -5,16 +5,32 @@ const apiKey = '348839-SimpleRe-U2PY34VF';
 const options = {
     method: 'GET',
     mode: 'no-cors'
+    // credentials: 'same-origin'
 }
 
 function getRecommendation() {
 
+    // searchFor, searchType, callback
+    
     const url = `https://tastedive.com/api/similar?q=the+smiths&k=${apiKey}`;
+
+    // https://cors-anywhere.herokuapp.com/
 
     fetch(url, options)
     .then(response => response.json())
     .then(responseJson => console.log(responseJson))
     // .catch(error => alert('Something went wrong.'))
+
+    //     let query = {
+    //         type: searchType,
+    //         k: apiKey,
+    //         q: searchFor,
+    //         limit: tasteDiveQueryLimit,
+    //         info: 1
+    //     };
+    
+    //     $.getJSON(url, query, callback);
+    // };
 }
 
 function watchForm() {
